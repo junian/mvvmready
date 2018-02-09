7dc725b9bcc91be7882eda112d3814461e3c081c
Junian <git@junian.net>
2018-02-09 17:44:22 +0700

    simpilfy onpropertychaged

--- src/MvvmReady.Standard/ViewModelBase.cs
+++ src/MvvmReady.Standard/ViewModelBase.cs
@@ -29,11 +29,7 @@ namespace MvvmReady
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
-            var changed = PropertyChanged;
-            if (changed == null)
-                return;
-
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
     }
